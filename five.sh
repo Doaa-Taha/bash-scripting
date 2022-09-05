80a91b4f2e9b9c53ed95b753e9264e7987daeee6
Doaa-Taha <88685245+Doaa-Taha@users.noreply.github.com>
2022-09-05 10:12:44 +0200

    finish the program scripting

--- five.sh
+++ five.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-# Program to tell a persons fortune
+# Program to run my other four programs
 
-echo -e "\n~~ Fortune Teller ~~\n"
-
-RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
-N=$(( RANDOM % 6 ))
-
-GET_FORTUNE(){
-  if [[ ! $1 ]]
-  then 
-    echo "Ask a yes or no question:"
-  else 
-    echo "Try again. Make sure it ends with a question mark:"
-  fi  
-  read QUESTION
-}
-
-GET_FORTUNE
-
-until [[ $QUESTION =~ \?$ ]]
-do
-  GET_FORTUNE again
-done
-
-echo -e "\n${RESPONSES[$N]}"
+./questionnaire.sh
+./countdown.sh 3
+./bingo.sh
+./fortune.sh
